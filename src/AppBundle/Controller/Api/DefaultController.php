2f505c8017a2cca7f611bae6d9932edcb8325465
Bulava Eduard <bulavaeduard@gmail.com>
2017-02-19 17:46:49 +0200

    api login system

--- src/AppBundle/Controller/Api/DefaultController.php
+++ src/AppBundle/Controller/Api/DefaultController.php
@@ -5,17 +5,35 @@ namespace AppBundle\Controller\Api;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/login", name="api_login")
      * @param Request $request
+     * @Route("/login", name="api_login")
+     * @return JsonResponse
      */
     public function loginAction(Request $request)
     {
+        $user = $this->getDoctrine()->getRepository('AppBundle:UserIntern')
+            ->findOneBy(['userName' => $request->get('login')]);
+        if (!$user){
+            return new JsonResponse(['message' => 'Bad credentials'], 403);
+        }
+        $result = $this->get('security.encoder_factory')
+            ->getEncoder($user)
+            ->isPasswordValid($user->getPassword(), $request->get('password'), null);
+        if (!$result){
+            return new JsonResponse(['message' => 'Bad credentials'], 403);
+        }
 
+        $token = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
+
+        //set token to user
+
+        return new JsonResponse(['X-AUTH-TOKEN' => $token]);
     }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -16,5 +17,30 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return [];
+    }
+
+    /**
+     * @Route("/login", name="login")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Method({"GET", "POST"})
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('', array(
+            'last_username' => $lastUsername, //$lastUsername,
+            'error' => $error,
+        ));
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
     }
 }
