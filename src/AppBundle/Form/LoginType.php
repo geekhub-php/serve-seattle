a7e9f6efd3dfbde682b068280d0e9e784c089551
Bulava Eduard <bulavaeduard@gmail.com>
2017-03-12 14:20:11 +0200

    api login handle by form

--- src/AppBundle/Form/LoginType.php
+++ src/AppBundle/Form/LoginType.php
@@ -8,6 +8,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LoginType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     * @SuppressWarnings("UnusedFormalParameter")
+     * After add new field in UserType need create
+     * offsetUnset() method from this field in Security controller
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
