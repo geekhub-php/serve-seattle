776f9be572e9f1921408be222a59f85d88783dc3
Olexandr Smagliy <smagolexandr@gmail.com>
2017-03-15 08:33:26 +0200

    install script upd

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ composer install;
 ./bin/console doctrine:database:create;
 ./bin/console doctrine:schema:create;
 ./bin/console hautelook:fixtures:load -n;
+
+./bin/console doctrine:database:create --env=test;
+./bin/console doctrine:schema:create --env=test;
+./bin/console hautelook:fixtures:load -n --env=test;
+
 npm install
 bower install
 gulp
