80c3d0b07d2aaf8d8bc6481a582a64922eb13222
Bulava Eduard <bulavaeduard@gmail.com>
2017-03-25 02:42:57 +0200

    aws SES works

--- src/AppBundle/Notification/EmailNotification.php
+++ src/AppBundle/Notification/EmailNotification.php
@@ -24,16 +24,13 @@ class EmailNotification
 
     public function sendNotification($email, $title = 'Hello!', $content = ' ')
     {
-        $message = \Swift_Message::newInstance();
+        $message = new \Swift_Message();
         $message
             ->setSubject('Hello Email')
             ->setFrom($this->from)
             ->setTo($email)
             ->setBody($this->twig->render('AppBundle:Email:email.html.twig', [
-                'title' => $title,
-                'content' => $content
-            ]),
-                'text/html');
+                'title' => $title, 'content' => $content]), 'text/html');
         $this->mailer->send($message);
     }
 }
