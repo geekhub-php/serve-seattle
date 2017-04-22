b530dc80b2a2fd00c5b7cfe303a546fbf4384262
Bulava Eduard <bulavaeduard@gmail.com>
2017-04-22 10:32:02 +0300

    image500

--- web-src/scripts/survey.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Dropdown menu
-document.getElementById('add-survey').addEventListener('click', function () {
-    document.getElementById('survey-list').classList.toggle('hidden');
-});
